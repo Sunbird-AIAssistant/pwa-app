7e924b2838b0d7d81caf297c7344012cb7dd763a
nidhikachhawa24 <nidhi_k@tekditechnologies.com>
2024-04-25 12:37:52 +0530

    created config file

---
+++ src/app/config.ts
@@ -0,0 +1,146 @@
+export const ConfigVariables = {
+    "tenantLogo": "assets/images/Story_bot.svg",
+    "siteName": "BEML Public School",
+    "siteByLine": "Listen, Learn & Grow !!!",
+    "splashImage": "assets/images/tekdi.png",
+    "headerFilters": [{
+        "defaultFilter": {
+            "id": "All",
+            "label": "All",
+            "query": "All",
+            "filters": "All"
+        },
+        "additionalFilters": [
+            {
+                "id": "Stories",
+                "label": "Stories",
+                "query": "Stories",
+                "filters": "Stories"
+            },
+            {
+                "id": "Toys and Games",
+                "label": "Toys and Games",
+                "query": "Toys and Games",
+                "filters": "Toys and Games"
+            },
+            {
+                "id": "learning",
+                "label": "Learning",
+                "query": "learning",
+                "filters": "learning"
+            },
+            {
+                "id": "online courses",
+                "label": "online courses",
+                "query": "online courses",
+                "filters": "online courses"
+            }
+        ]
+    }],
+    "languages": [
+        {
+            "id": "as",
+            "label": "অসমীয়া",
+            "default": true
+        },
+        {
+            "id": "bn",
+            "label": "বাংলা",
+            "default": false
+        },
+        {
+            "id": "bo",
+            "label": "बोडो",
+            "default": false
+        },
+        {
+            "id": "do",
+            "label": "डोगरी",
+            "default": true
+        },
+        {
+            "id": "en",
+            "label": "English",
+            "default": false
+        },
+        {
+            "id": "gu",
+            "label": "ગુજરાતી",
+            "default": false
+        },
+        {
+            "id": "hi",
+            "label": "हिंदी",
+            "default": true
+        },
+        {
+            "id": "kn",
+            "label": "ಕನ್ನಡ",
+            "default": false
+        },
+        {
+            "id": "ko",
+            "label": "कोंकणी",
+            "default": false
+        },
+        {
+            "id": "ks",
+            "label": "ಕಶ್ಮೀರಿ",
+            "default": false
+        },
+        {
+            "id": "ma",
+            "label": "मैथिली",
+            "default": true
+        },
+        {
+            "id": "ml",
+            "label": "മലയാളം",
+            "default": false
+        },
+        {
+            "id": "mn",
+            "label": "মণিপুরী",
+            "default": false
+        }
+    ],
+    // "apiEndPoint": "/dsep/search",
+    // "bookmark": "BookMark",
+    // "rating": "Rating",
+    // "share": "Share",
+    // "pagination": "10",
+    // "orderBy": "",
+    // "filterBy": "default",
+    "footerText": "Developed with ❤️ by Tekdi Technologies ",
+    "headerColor": "yellow",
+    "headerFontSize": "16",
+    "footerColor": "yellow",
+    "footerFontSize": "16",
+    // "positionByLine": "false",
+    // "positionLogo": "false",
+    // "positionSiteName": "false",
+    // "labelTitle": "शीर्षक",
+    // "labelAuthor": "लेखक",
+    // "labelDesc": "Desc",
+    // "labelRating": "Rating",
+    // "displayOrder": {
+    //     "title": "3",
+    //     "author": "1",
+    //     "description": "2",
+    //     "publisher": "4"
+    // },
+    // "filters": {
+    //     "minAge": "",
+    //     "language": "",
+    //     "contentType": ""
+    // },
+    // "searchFilters":{
+    //         "age":["1","2","3","4","5","6","7","8","9","10"],
+    //         "language":["English","Hindi","Gujarati","Tamil","Kannada"],
+    //         "type":["Video","Audio","Read","Others"]
+    // },
+    // "contentSource":{
+    //         "source":["onest","SB"]
+    // },
+    // "logo": "photo-1492144534655-ae79c964c9d71699539070789.avif"
+}
