12732ece935bfcc419ad0711987f79e26d64d22a
nidhikachhawa24 <nidhi_k@tekditechnologies.com>
2024-05-08 10:11:47 +0530

    removed the confirm api call

--- configuration/environment.prod.ts
+++ configuration/environment.prod.ts
@@ -4,19 +4,20 @@ export const environment = {
 
 export const config = {
   api: {
-    BASE_URL: 'https://dev.ejp.sunbird.org/',
-    CONFIG: 'api/content/v1/config/read',
-    PAGE_SEARCH_API: 'api/content/v1/page/search',
+    BASE_URL: 'https://kahani-api.tekdinext.com/',
+    CONFIG: 'seeker/configuration',
+    PAGE_SEARCH_API: 'content/search',
     CONTEXT_SEARCH: 'api/aiutility/v1/context',
-    CONTENT_SEARCH_API: 'api/content/v1/content/search',
+    CONTENT_SEARCH_API: 'content/search',
     TELEMETRY_SYNC: 'api/telemetry/v1',
     BOT_QUERY_API: 'v1/query',
-    SEARCH_API: 'api/content/v1/search',
-    BOT_SAKHI_API_PATH: 'api/storybot/v1/query',
+    SEARCH_API: 'content/search',
+    CONFIRM_API: 'confirm',
+    BOT_SAKHI_API_PATH: 'v1/query',
     BOT_ACTIVITY_API_PATH: 'api/activitybot/v1/query',
     REGISTER_DEVICE_API_PATH: 'api/registerMobileDevice',
-    KEY: '',
-    SECRET: ''
+    KEY: 'myjp-0.1',
+    SECRET: '7tVOEu0xj0zYQiYtCYbauEkNC3NBXCpM'
   },
   telmetry: {
     PRODUCER_ID: 'dev.ejp.mobileapp',
