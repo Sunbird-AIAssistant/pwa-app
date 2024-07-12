30e37b9ce26b39c789cdaf5fff9afec423df7ea0
Pratiksha Khandagale <pratiksha_k@techjoomla.com>
2024-07-12 16:42:00 +0530

    www updated

--- www/assets/installPrompt.js
+++ www/assets/installPrompt.js
@@ -2,30 +2,63 @@
 
 let deferredPrompt;
 
+function showInstallDialog() {
+  const overlay = document.getElementById('overlay');
+  const installDialog = document.getElementById('installDialog');
+  overlay.style.display = 'block';
+  installDialog.style.display = 'block';
+}
+
+function hideInstallDialog() {
+  const overlay = document.getElementById('overlay');
+  const installDialog = document.getElementById('installDialog');
+  overlay.style.display = 'none';
+  installDialog.style.display = 'none';
+}
+
+function isPWAInstalled() {
+  const isIOS = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test(userAgent);
+  };
+
+  const isInStandaloneMode = () => (
+    window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone
+  );
+
+  return isIOS() ? isInStandaloneMode() : window.matchMedia('(display-mode: standalone)').matches;
+}
+
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
   // Stash the event so it can be triggered later
   deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  const installButton = document.getElementById('installButton');
-  installButton.style.display = 'block';
 
-  installButton.addEventListener('click', () => {
-    // Hide the install button
-    installButton.style.display = 'none';
-    // Show the install prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the install prompt');
-      } else {
-        console.log('User dismissed the install prompt');
-      }
-      deferredPrompt = null;
-    });
+  // Check if the PWA is already installed
+  if (!isPWAInstalled()) {
+    // Show the custom install dialog
+    showInstallDialog();
+  }
+});
+
+document.querySelector('#installDialog .install').addEventListener('click', () => {
+  hideInstallDialog();
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+    deferredPrompt = null;
   });
+});
+
+document.querySelector('#installDialog .cancel').addEventListener('click', () => {
+  hideInstallDialog();
 });
 
 window.addEventListener('appinstalled', () => {
