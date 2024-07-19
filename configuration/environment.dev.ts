00e4d5571f104dc284e78b2063dbf5843f42dcd2
Pratiksha Khandagale <pratiksha_k@techjoomla.com>
2024-07-19 11:52:21 +0530

    remove unwanted code

--- configuration/environment.dev.ts
+++ configuration/environment.dev.ts
@@ -1,23 +1,25 @@
 export const environment = {
   production: true
 };
-
 export const config = {
   api: {
-    BASE_URL: 'http://152.67.162.156:9000/',
-    BASE_URL_NEW: 'https://dev.ejp.sunbird.org/',
-    SEARCH_BASE_URL: 'http://129.154.42.158:8000/',
-    BOT_BASE_URL: 'http://144.24.130.223:7081/',
-    STORY_BOT_BASE_URL: 'http://152.67.183.46:7081/',
-    CONFIG: 'v1/config/read',
-    PAGE_SEARCH_API: 'v1/page/search',
-    CONTEXT_SEARCH: 'v1/context',
-    CONTENT_SEARCH_API: 'v1/content/search',
-    TELEMETRY_SYNC: 'v1/telemetry',
+    BASE_URL: 'https://school-api-dev.tekdinext.com/',
+    BASE_URL_BOT: 'https://school-openai.tekdinext.com/',
+    TELEMETRY_BASE_URL: 'https://school-dev.tekdinext.com/',
+    CONFIG: 'seeker/configuration',
+    PAGE_SEARCH_API: 'content/search',
+    CONTEXT_SEARCH: 'api/aiutility/v1/context',
+    CONTENT_SEARCH_API: 'content/search',
+    TELEMETRY_SYNC: 'telemetry/v1/telemetry',
     BOT_QUERY_API: 'v1/query',
-    SEARCH_API: 'api/content/v1/search',
+    SEARCH_API: 'content/search',
+    CONFIRM_API: 'confirm',
     BOT_SAKHI_API_PATH: 'v1/query',
-    BOT_ACTIVITY_API_PATH: 'v1/query'
+    BOT_ACTIVITY_API_PATH: 'api/activitybot/v1/query',
+
+    REGISTER_DEVICE_API_PATH: 'api/registerMobileDevice',
+    KEY: 'myjp-0.1',
+    SECRET: '7tVOEu0xj0zYQiYtCYbauEkNC3NBXCpM'
   },
   telmetry: {
     PRODUCER_ID: 'dev.ejp.mobileapp',
